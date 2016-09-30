cb1f56e99251c1e5089fb602b33be5148b343e0a
Sabareesh Krishnan <krisshsabareesh@gmail.com>
2016-09-30 15:38:22 +0530

    Huffman Code Partial - Using NODEJS

---
+++ huffman/huffman.js
@@ -0,0 +1,101 @@
+`NODEJS`
+
+codes = {}
+function frequency(str) {
+
+  var freqs = {}
+  console.log('Running...Frequency')
+  console.log('Frequency')
+  for(var i=0; i < str.length; i++) {
+    if (freqs[str[i]]) 
+      freqs[str[i]] = freqs[str[i]] + 1
+    else
+      freqs[str[i]] = 1
+  }
+  console.log(freqs)
+  return(freqs)
+}
+
+
+function sortFreq(freqs) {
+  
+  var tuples = []
+  for(key in freqs) { 
+    tuples.push(Array(freqs[key], key))
+  }
+  console.log(tuples) 
+  return(tuples.sort)
+}
+
+
+function buildTree(tuples) {
+
+  while(tuples.length > 1) {
+    var leastTwo  = tuples.slice(0,2)             
+    var theRest = tuples.slice(2,tuples.length)
+    var combFreq = leastTwo[0][0] + leastTwo[1][0]
+    tuples = theRest + [(combFreq, leastTwo)]
+    tuples.sort()
+  }
+  console.log(tuples)
+  return(tuples[0])
+}
+
+
+function trimTree(tree) {
+
+  p = tree[1]
+  if( typeof p == typeof 'str') 
+    return p
+  else
+    return(trimTree(p[0]), trimTree(p[1]))
+}
+
+
+function assignCodes(node, pat) {
+
+  if( typeof node == 'str') { 
+    codes[node] = pat
+  }
+  else { 
+    assignCodes(node[0], pat+"0")
+    assignCodes(node[1],pat+"1") 
+  }
+}
+
+
+function encode (str) {
+
+    var output = ""
+    for(ch in str) 
+      output += codes[ch]
+    return(output)
+}
+
+
+function decode (tree, str) {
+
+    var output = ""
+    var p = tree
+    for( bit in str) {
+        if( bit == '0') 
+          p = p[0]     
+        else
+          p = p[1]     
+        if( typeof p == 'str') {
+            output += p              
+            p = tree  
+        }
+    }               
+    return(output)
+}
+
+
+
+var freqs = frequency('aaababacadaeef');
+console.log('After Frequency : ' + freqs)
+var tuples = sortFreq(freqs)
+console.log('After sorting Frequency : ' + tuples)
+var tree = buildTree(tuples)
+console.log('After Building Tree : ' + tree)
+
